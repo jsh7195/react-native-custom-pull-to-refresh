973cf0939b37824a71bc4208dc5965acc6f34798
shji <jsh7195gg@gmail.com>
2024-03-20 12:39:46 +0900

    build : new build

--- dist/src/PullToRefreshAOS.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { IPullToRefreshChild } from './PullToRefresh';
-declare const PullToRefreshAOS: (props: IPullToRefreshChild) => React.JSX.Element;
-export default PullToRefreshAOS;
--- dist/src/PullToRefreshIOS.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { IPullToRefreshChild } from './PullToRefresh';
-declare const PullToRefreshIOS: (props: IPullToRefreshChild) => React.JSX.Element;
-export default PullToRefreshIOS;
