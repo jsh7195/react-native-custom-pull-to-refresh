1d2bdf9afb0fa16878cab2b84d916a94c22c2346
shji <jsh7195gg@gmail.com>
2024-03-20 12:32:21 +0900

    feat : ignore change

---
+++ dist/src/PullToRefresh.d.ts
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FlatListProps, NativeSyntheticEvent, NativeScrollEvent } from "react-native";
+import { SharedValue } from 'react-native-reanimated';
+export interface IPullToRefreshComponentProps extends Omit<FlatListProps<any>, "data" | "renderItem" | "ListHeaderComponent"> {
+    onRefresh?: () => void;
+    onScroll?: ((event: NativeSyntheticEvent<NativeScrollEvent>) => void) | undefined;
+    THRESHOLD?: number;
+    RefreshComponent?: React.Element | null | undefined;
+    ListHeaderComponent?: React.Element | null | undefined;
+    refreshPosition?: "top" | "bottom";
+    data: any[];
+    renderItem: any;
+    contentContainerStyle?: any;
+    useOpacity?: boolean;
+}
+export interface IPullToRefreshChild extends IPullToRefreshComponentProps {
+    handleRefreshCompleteRefrshApi: () => void;
+    handleRefreshComplete: () => void;
+    THRESHOLD: number;
+    scrollY: SharedValue<number>;
+    enablePullToRefresh: SharedValue<boolean>;
+    scrollRef: React.MutableRefObject<null>;
+}
+export interface IPullToRefreshFunction {
+    goTop: () => void;
+}
---
+++ dist/src/PullToRefreshAOS.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { IPullToRefreshChild } from './PullToRefresh';
+declare const PullToRefreshAOS: (props: IPullToRefreshChild) => React.JSX.Element;
+export default PullToRefreshAOS;
