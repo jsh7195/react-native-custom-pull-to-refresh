4f46710ee0ebe72e882aef540a315079995003cd
shji <jsh7195gg@gmail.com>
2024-03-20 12:39:56 +0900

    build

---
+++ dist/src/PullToRefreshFlatList.d.ts
@@ -0,0 +1,4 @@
+import React from "react";
+import { IPullToRefreshComponentProps, IPullToRefreshFunction } from "./PullToRefresh";
+declare const PullToRefreshFlatList: React.ForwardRefExoticComponent<IPullToRefreshComponentProps & React.RefAttributes<IPullToRefreshFunction>>;
+export default PullToRefreshFlatList;
